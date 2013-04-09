a8369b9b7b59c7044be0d761a94fada949c1e38e
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2013-04-09 10:59:22 -0700

    implemented ordering

--- proj3/sgemm-small.c
+++ proj3/sgemm-small.c
@@ -13,24 +13,26 @@ int BLOCK_WIDTH = 16;
 void sgemm( int m, int n, int d, float *A, float *C )
 {
  
-for( int k = 0; k < m; k++ ) {
 	for( int j = 0; j < n; j++ ) {
-      	for( int i = 0; i < (n - (n % 4)); i += 4 ) {
-			// C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
-			// Unrolling i-loop
-			__m128 value1 = _mm_loadu_ps (A + i+k*n);
-			__m128 value2 = _mm_load1_ps (A + j*(n+1)+k*(n));
-			__m128 intValue = _mm_mul_ps (value1, value2);
-			__m128 oldValue = _mm_loadu_ps (C + i+j*n);
-			__m128 newValue = _mm_add_ps (intValue, Value);
-			_mm_storeu_ps(C + i+j*n, newValue);
-		}
-		for ( int i = (n - (n % 4)); i < n; i++)
-			C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
-		}
-	} 
+		for( int k = 0; k < m; k++ ) {
+	      	for( int i = 0; i < (n - (n % 4)); i += 4 ) {
+				// C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
+				// Unrolling i-loop
+				__m128 value1 = _mm_loadu_ps (A + i+k*n);
+				__m128 value2 = _mm_load1_ps (A + j*(n+1)+k*(n));
+				__m128 intValue = _mm_mul_ps (value1, value2);
+				__m128 oldValue = _mm_loadu_ps (C + i+j*n);
+				__m128 newValue = _mm_add_ps (intValue, oldValue);
+				_mm_storeu_ps(C + i+j*n, newValue);
+			}
+			for ( int i = (n - (n % 4)); i < n; i++)
+				C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
+			}
+		} 
 }
 
 /* step1: loop unrolling with ssid instructions. */
+
+/*ordering*/
 
 /* Padding or edge cases? */
