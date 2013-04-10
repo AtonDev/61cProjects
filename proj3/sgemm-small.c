98fa3bcc2bcbbc8b65aee41bfc4e78f071beb567
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2013-04-10 15:21:18 -0700

    new faster ordering but fringe cases are not working

--- proj3/sgemm-small.c
+++ proj3/sgemm-small.c
@@ -10,17 +10,16 @@
 
 /* Naive operation. M is the size of the strip. N is the number of rows of A. 
    N+D is the number of colums of A. */
-void sgemm( int m, int n, int d, float *A, float *C )
+/*void sgemm( int m, int n, int d, float *A, float *C )
 {
 	for( int j = 0; j < n; j++ ) {
 		for( int k = 0; k < m; k++ ) {
 			__m128 value2 = _mm_load1_ps (A + j*(n+1)+k*(n));
-			__m128 value1, intValue, oldValue, newValue;
 			for( int i = 0; i < (n - (n % 16)); i += 16 ) {
-				value1 = _mm_loadu_ps (A + i+k*n);
-				intValue = _mm_mul_ps (value1, value2);
-				oldValue = _mm_loadu_ps (C + i+j*n);
-				newValue = _mm_add_ps (intValue, oldValue);
+				__m128 value1 = _mm_loadu_ps (A + i+k*n);
+				__m128 intValue = _mm_mul_ps (value1, value2);
+				__m128 oldValue = _mm_loadu_ps (C + i+j*n);
+				__m128 newValue = _mm_add_ps (intValue, oldValue);
 				_mm_storeu_ps(C + i+j*n, newValue);
 				value1 = _mm_loadu_ps (A + (i+4)+k*n);
 				intValue = _mm_mul_ps (value1, value2);
@@ -45,17 +44,89 @@ void sgemm( int m, int n, int d, float *A, float *C )
 				__m128 newValue = _mm_add_ps (intValue, oldValue);
 				_mm_storeu_ps(C + i+j*n, newValue);
 			}
-			for ( int i = (n - (n % 4)); i < n; i++)
+			for ( int i = (n - (n % 4)); i < n; i++) {
 				C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
-			}
-		} 
+			}	
+		}
+	} 
+}*/
+
+
+void sgemm( int m, int n, int d, float *A, float *C )
+{ 
+    for( int j = 0; j < n; j++ ) {
+    	for( int i = 0; i < (n - (n % 4)); i += 4 ){
+    		__m128 newValue = _mm_loadu_ps (C + i+j*n);
+    		for( int k = 0; k < (m - (m % 4)); k += 4 ) {
+    			__m128 value2 = _mm_load1_ps (A + j*(n+1)+k*(n));
+    			__m128 value1 = _mm_loadu_ps (A + i+k*n);
+				__m128 intValue = _mm_mul_ps (value1, value2);
+				newValue = _mm_add_ps (intValue, newValue);
+				value2 = _mm_load1_ps (A + j*(n+1)+(k+1)*(n));
+    			value1 = _mm_loadu_ps (A + i+(k+1)*n);
+				intValue = _mm_mul_ps (value1, value2);
+				newValue = _mm_add_ps (intValue, newValue);
+				value2 = _mm_load1_ps (A + j*(n+1)+(k+2)*(n));
+    			value1 = _mm_loadu_ps (A + i+(k+2)*n);
+				intValue = _mm_mul_ps (value1, value2);
+				newValue = _mm_add_ps (intValue, newValue);
+				value2 = _mm_load1_ps (A + j*(n+1)+(k+3)*(n));
+    			value1 = _mm_loadu_ps (A + i+(k+3)*n);
+				intValue = _mm_mul_ps (value1, value2);
+				newValue = _mm_add_ps (intValue, newValue);
+    		}
+    		for( int k = (m - (m % 4)); k < m; k += 1) {
+    			__m128 value2 = _mm_load1_ps (A + j*(n+1)+k*(n));
+    			__m128 value1 = _mm_loadu_ps (A + i+k*n);
+				__m128 intValue = _mm_mul_ps (value1, value2);
+				newValue = _mm_add_ps (intValue, newValue);
+    		}
+    		_mm_storeu_ps(C + i+j*n, newValue);
+		}
+		for( int i = (n - (n % 4)); i < n; i++ ) {
+			/*__m128 newValue = _mm_loadu_ps (C + i+j*n);
+    		for( int k = 0; k < (m - (m % 4)); k += 4 ) {
+    			__m128 value2 = _mm_load1_ps (A + j*(n+1)+k*(n));
+    			__m128 value1 = _mm_loadu_ps (A + i+k*n);
+				__m128 intValue = _mm_mul_ps (value1, value2);
+				newValue = _mm_add_ps (intValue, newValue);
+				value2 = _mm_load1_ps (A + j*(n+1)+(k+1)*(n));
+    			value1 = _mm_loadu_ps (A + i+(k+1)*n);
+				intValue = _mm_mul_ps (value1, value2);
+				newValue = _mm_add_ps (intValue, newValue);
+				value2 = _mm_load1_ps (A + j*(n+1)+(k+2)*(n));
+    			value1 = _mm_loadu_ps (A + i+(k+2)*n);
+				intValue = _mm_mul_ps (value1, value2);
+				newValue = _mm_add_ps (intValue, newValue);
+				value2 = _mm_load1_ps (A + j*(n+1)+(k+3)*(n));
+    			value1 = _mm_loadu_ps (A + i+(k+3)*n);
+				intValue = _mm_mul_ps (value1, value2);
+				newValue = _mm_add_ps (intValue, newValue);
+    		}
+    		for( int k = (m - (m % 4)); k < m; k += 1) {
+    			__m128 value2 = _mm_load1_ps (A + j*(n+1)+k*(n));
+    			__m128 value1 = _mm_loadu_ps (A + i+k*n);
+				__m128 intValue = _mm_mul_ps (value1, value2);
+				newValue = _mm_add_ps (intValue, newValue);
+    		}
+    		_mm_storeu_ps(C + i+j*n, newValue);*/
+		}
+	}
 }
+
+
+
+
+
+
+
+
 // C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
 /* s1: vectorizing with sse instructions. -- implemented
    s2: loop ordering -- implmented 
-   s3: register blocking with local variable -- no optimisation?
+   s3: register blocking with local variable -- implemented
    s4: implmenting loop unrolling --
-   s5: according to whether we have sufficient performance cahche blocking -- 
+   s5: according to whether we have sufficient performance cache blocking -- 
    s6: compiler tricks? -- 
    */
 
