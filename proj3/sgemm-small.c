b258f4d1b3eb845f10705de6d27c04b694b6fdfc
EntilZha <ski.rodriguez@gmail.com>
2013-04-11 08:45:01 -0700

    up to 6.3

--- proj3/sgemm-small.c
+++ proj3/sgemm-small.c
@@ -51,9 +51,43 @@
 	} 
 }*/
 
-
+// C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
 void sgemm( int m, int n, int d, float *A, float *C )
 { 
+	for (int k = 0; k < m; k++) {
+		for (int j = 0; j < n; j +=4) {
+			__m128 transposeValue0 = _mm_load1_ps(A + j*(n+1) + k * n);
+			__m128 transposeValue1 = _mm_load1_ps(A + (j+1)*(n+1) + k * n);
+			__m128 transposeValue2 = _mm_load1_ps(A + (j+2)*(n+1) + k * n);
+			__m128 transposeValue3 = _mm_load1_ps(A + (j+3)*(n+1) + k * n);
+			for (int i = 0; i < n; i += 4) {
+				__m128 stripValues = _mm_loadu_ps(A + i + k * n);
+				__m128 computeValue = _mm_loadu_ps(C + i + j * n);
+				__m128 mulValues = _mm_mul_ps(stripValues, transposeValue0);
+				computeValue = _mm_add_ps(computeValue, mulValues);
+				_mm_storeu_ps(C + i + j * n, computeValue);
+
+				stripValues = _mm_loadu_ps(A + i + k * n);
+				computeValue = _mm_loadu_ps(C + i + (j+1) * n);
+				mulValues = _mm_mul_ps(stripValues, transposeValue1);
+				computeValue = _mm_add_ps(computeValue, mulValues);
+				_mm_storeu_ps(C + i + (j+1) * n, computeValue);
+
+				stripValues = _mm_loadu_ps(A + i + k * n);
+				computeValue = _mm_loadu_ps(C + i + (j+2) * n);
+				mulValues = _mm_mul_ps(stripValues, transposeValue2);
+				computeValue = _mm_add_ps(computeValue, mulValues);
+				_mm_storeu_ps(C + i + (j+2) * n, computeValue);
+
+				stripValues = _mm_loadu_ps(A + i + k * n);
+				computeValue = _mm_loadu_ps(C + i + (j+3) * n);
+				mulValues = _mm_mul_ps(stripValues, transposeValue3);
+				computeValue = _mm_add_ps(computeValue, mulValues);
+				_mm_storeu_ps(C + i + (j+3) * n, computeValue);
+			}
+		}
+	} 
+    /*
     for( int j = 0; j < n; j++ ) {
     	for( int i = 0; i < (n - (n % 4)); i += 4 ){
     		__m128 newValue = _mm_loadu_ps (C + i+j*n);
@@ -96,7 +130,7 @@ void sgemm( int m, int n, int d, float *A, float *C )
     		}
     		C[i+j*n] = sum;
     	}
-	}
+	}*/
 }
 
 
