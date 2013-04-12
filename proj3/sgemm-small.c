57013315faa58fc190136aca702e0e0937ede677
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2013-04-11 17:20:55 -0700

    moving aroung comments

--- proj3/sgemm-small.c
+++ proj3/sgemm-small.c
@@ -2,92 +2,9 @@
 #include <emmintrin.h>
 #include <xmmintrin.h>
 
-/* Size of blocks. Cache line / sizeof float. 64 bytes / 4 bytes = 16 floats. */
-#define BLOCK_WIDTH 16
 
-
-
-
-/* Naive operation. M is the size of the strip. N is the number of rows of A. 
-   N+D is the number of colums of A. */
-/*void sgemm( int m, int n, int d, float *A, float *C )
-{
-	for( int j = 0; j < n; j++ ) {
-		for( int k = 0; k < m; k++ ) {
-			__m128 value2 = _mm_load1_ps (A + j*(n+1)+k*(n));
-			for( int i = 0; i < (n - (n % 16)); i += 16 ) {
-				__m128 value1 = _mm_loadu_ps (A + i+k*n);
-				__m128 intValue = _mm_mul_ps (value1, value2);
-				__m128 oldValue = _mm_loadu_ps (C + i+j*n);
-				__m128 newValue = _mm_add_ps (intValue, oldValue);
-				_mm_storeu_ps(C + i+j*n, newValue);
-				value1 = _mm_loadu_ps (A + (i+4)+k*n);
-				intValue = _mm_mul_ps (value1, value2);
-				oldValue = _mm_loadu_ps (C + (i+4)+j*n);
-				newValue = _mm_add_ps (intValue, oldValue);
-				_mm_storeu_ps(C + (i+4)+j*n, newValue);
-				value1 = _mm_loadu_ps (A + (i+8)+k*n);
-				intValue = _mm_mul_ps (value1, value2);
-				oldValue = _mm_loadu_ps (C + (i+8)+j*n);
-				newValue = _mm_add_ps (intValue, oldValue);
-				_mm_storeu_ps(C + (i+8)+j*n, newValue);
-				value1 = _mm_loadu_ps (A + (i+12)+k*n);
-				intValue = _mm_mul_ps (value1, value2);
-				oldValue = _mm_loadu_ps (C + (i+12)+j*n);
-				newValue = _mm_add_ps (intValue, oldValue);
-				_mm_storeu_ps(C + (i+12)+j*n, newValue);
-			}
-	      	for( int i = (n - (n % 16)); i < (n - (n % 4)); i += 4 ) {
-				__m128 value1 = _mm_loadu_ps (A + i+k*n);
-				__m128 intValue = _mm_mul_ps (value1, value2);
-				__m128 oldValue = _mm_loadu_ps (C + i+j*n);
-				__m128 newValue = _mm_add_ps (intValue, oldValue);
-				_mm_storeu_ps(C + i+j*n, newValue);
-			}
-			for ( int i = (n - (n % 4)); i < n; i++) {
-				C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
-			}	
-		}
-	} 
-}*/
-
-// C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
 void sgemm( int m, int n, int d, float *A, float *C )
-{ 
-	/*
-	for (int k = 0; k < m; k++) {
-		for (int j = 0; j < n; j +=4) {
-			__m128 transposeValue0 = _mm_load1_ps(A + j*(n+1) + k * n);
-			__m128 transposeValue1 = _mm_load1_ps(A + (j+1)*(n+1) + k * n);
-			__m128 transposeValue2 = _mm_load1_ps(A + (j+2)*(n+1) + k * n);
-			__m128 transposeValue3 = _mm_load1_ps(A + (j+3)*(n+1) + k * n);
-			for (int i = 0; i < n; i += 4) {
-				__m128 stripValues = _mm_loadu_ps(A + i + k * n);
-				__m128 computeValue = _mm_loadu_ps(C + i + j * n);
-				__m128 mulValues = _mm_mul_ps(stripValues, transposeValue0);
-				computeValue = _mm_add_ps(computeValue, mulValues);
-				_mm_storeu_ps(C + i + j * n, computeValue);
-
-				stripValues = _mm_loadu_ps(A + i + k * n);
-				computeValue = _mm_loadu_ps(C + i + (j+1) * n);
-				mulValues = _mm_mul_ps(stripValues, transposeValue1);
-				computeValue = _mm_add_ps(computeValue, mulValues);
-				_mm_storeu_ps(C + i + (j+1) * n, computeValue);
-
-				stripValues = _mm_loadu_ps(A + i + k * n);
-				computeValue = _mm_loadu_ps(C + i + (j+2) * n);
-				mulValues = _mm_mul_ps(stripValues, transposeValue2);
-				computeValue = _mm_add_ps(computeValue, mulValues);
-				_mm_storeu_ps(C + i + (j+2) * n, computeValue);
-
-				stripValues = _mm_loadu_ps(A + i + k * n);
-				computeValue = _mm_loadu_ps(C + i + (j+3) * n);
-				mulValues = _mm_mul_ps(stripValues, transposeValue3);
-				computeValue = _mm_add_ps(computeValue, mulValues);
-				_mm_storeu_ps(C + i + (j+3) * n, computeValue);
-			}
-		}
-	} */
+{
 	for (int j = 0; j < n; j++) {
 		for (int k = 0; k < m; k += 4) {
 			float* basej = A + j * (n + 1);
@@ -138,7 +55,12 @@ void sgemm( int m, int n, int d, float *A, float *C )
 			}
 		}
 	}
-    /*
+    
+}
+
+
+
+/*
     for( int j = 0; j < n; j++ ) {
     	for( int i = 0; i < (n - (n % 4)); i += 4 ){
     		__m128 newValue = _mm_loadu_ps (C + i+j*n);
@@ -182,13 +104,86 @@ void sgemm( int m, int n, int d, float *A, float *C )
     		C[i+j*n] = sum;
     	}
 	}*/
-}
 
+/* Naive operation. M is the size of the strip. N is the number of rows of A. 
+   N+D is the number of colums of A. */
+/*void sgemm( int m, int n, int d, float *A, float *C )
+{
+	for( int j = 0; j < n; j++ ) {
+		for( int k = 0; k < m; k++ ) {
+			__m128 value2 = _mm_load1_ps (A + j*(n+1)+k*(n));
+			for( int i = 0; i < (n - (n % 16)); i += 16 ) {
+				__m128 value1 = _mm_loadu_ps (A + i+k*n);
+				__m128 intValue = _mm_mul_ps (value1, value2);
+				__m128 oldValue = _mm_loadu_ps (C + i+j*n);
+				__m128 newValue = _mm_add_ps (intValue, oldValue);
+				_mm_storeu_ps(C + i+j*n, newValue);
+				value1 = _mm_loadu_ps (A + (i+4)+k*n);
+				intValue = _mm_mul_ps (value1, value2);
+				oldValue = _mm_loadu_ps (C + (i+4)+j*n);
+				newValue = _mm_add_ps (intValue, oldValue);
+				_mm_storeu_ps(C + (i+4)+j*n, newValue);
+				value1 = _mm_loadu_ps (A + (i+8)+k*n);
+				intValue = _mm_mul_ps (value1, value2);
+				oldValue = _mm_loadu_ps (C + (i+8)+j*n);
+				newValue = _mm_add_ps (intValue, oldValue);
+				_mm_storeu_ps(C + (i+8)+j*n, newValue);
+				value1 = _mm_loadu_ps (A + (i+12)+k*n);
+				intValue = _mm_mul_ps (value1, value2);
+				oldValue = _mm_loadu_ps (C + (i+12)+j*n);
+				newValue = _mm_add_ps (intValue, oldValue);
+				_mm_storeu_ps(C + (i+12)+j*n, newValue);
+			}
+	      	for( int i = (n - (n % 16)); i < (n - (n % 4)); i += 4 ) {
+				__m128 value1 = _mm_loadu_ps (A + i+k*n);
+				__m128 intValue = _mm_mul_ps (value1, value2);
+				__m128 oldValue = _mm_loadu_ps (C + i+j*n);
+				__m128 newValue = _mm_add_ps (intValue, oldValue);
+				_mm_storeu_ps(C + i+j*n, newValue);
+			}
+			for ( int i = (n - (n % 4)); i < n; i++) {
+				C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
+			}	
+		}
+	} 
+}*/
 
+// C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
+/*void sgemm( int m, int n, int d, float *A, float *C )
+{ 
+	for (int k = 0; k < m; k++) {
+		for (int j = 0; j < n; j +=4) {
+			__m128 transposeValue0 = _mm_load1_ps(A + j*(n+1) + k * n);
+			__m128 transposeValue1 = _mm_load1_ps(A + (j+1)*(n+1) + k * n);
+			__m128 transposeValue2 = _mm_load1_ps(A + (j+2)*(n+1) + k * n);
+			__m128 transposeValue3 = _mm_load1_ps(A + (j+3)*(n+1) + k * n);
+			for (int i = 0; i < n; i += 4) {
+				__m128 stripValues = _mm_loadu_ps(A + i + k * n);
+				__m128 computeValue = _mm_loadu_ps(C + i + j * n);
+				__m128 mulValues = _mm_mul_ps(stripValues, transposeValue0);
+				computeValue = _mm_add_ps(computeValue, mulValues);
+				_mm_storeu_ps(C + i + j * n, computeValue);
 
+				stripValues = _mm_loadu_ps(A + i + k * n);
+				computeValue = _mm_loadu_ps(C + i + (j+1) * n);
+				mulValues = _mm_mul_ps(stripValues, transposeValue1);
+				computeValue = _mm_add_ps(computeValue, mulValues);
+				_mm_storeu_ps(C + i + (j+1) * n, computeValue);
 
+				stripValues = _mm_loadu_ps(A + i + k * n);
+				computeValue = _mm_loadu_ps(C + i + (j+2) * n);
+				mulValues = _mm_mul_ps(stripValues, transposeValue2);
+				computeValue = _mm_add_ps(computeValue, mulValues);
+				_mm_storeu_ps(C + i + (j+2) * n, computeValue);
 
-
+				stripValues = _mm_loadu_ps(A + i + k * n);
+				computeValue = _mm_loadu_ps(C + i + (j+3) * n);
+				mulValues = _mm_mul_ps(stripValues, transposeValue3);
+				computeValue = _mm_add_ps(computeValue, mulValues);
+				_mm_storeu_ps(C + i + (j+3) * n, computeValue);
+			}
+		}
+	} */
 
 
 // C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
